3dec0a1afebe9f99e2f8a27bd1f0041a061d40b0
olushola oludipe <shotown101@gmail.com>
2022-03-02 21:49:57 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
     const qwerty = document.getElementById('qwerty');
     const phrase = document.getElementById('phrase');
     const startBtn =document.querySelector('.btn__reset');
+    const qwertyButton = document.q
     let missed = 0;
     const phrases= 
     ['shola is a good boy',
@@ -14,6 +15,35 @@
         let randomNumber = Math.floor(Math.random() * arr.length);
         return arr[randomNumber].split("");
     }
+
+    function addPhraseToDisplay(arr){
+        // do stuff any arr that is passed in, and add to `#phrase ul`
+        const ul = document.querySelector('#phrase ul');
+        for(let i = 0; i < arr.length; i++){
+            const li = document.createElement('li')
+            li.textContent = arr[i]
+            if(li.textContent !== ' '){
+                li.className = 'letter';
+                ul.appendChild(li);
+
+                }
+           
+            }        
+        }
+    
+        function checkLetter(){
+            let match = qwerty 
+            const checkLetter = document.getElementsByClassName('letter')
+            console.log(checkLetter);
+            
+        }
+
+        const q = getRandomPhraseAsArray(phrases)
+        addPhraseToDisplay(q);
+        checkLetter();
+            
+        
+ 
    
 
 
