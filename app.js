63d2b1388b340a8be36591c7213daddd01416b4b
olushola oludipe <shotown101@gmail.com>
2022-03-06 12:09:30 -0500

    updated the checkWin() and final push

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
     const qwerty = document.getElementById('qwerty');
     const phrase = document.getElementById('phrase');
     const startBtn =document.querySelector('.btn__reset');
-    const qwertyButton = document.q
+    const overlay =document.getElementById('overlay');
+
     let missed = 0;
     const phrases= 
     ['shola is a good boy',
@@ -30,14 +31,20 @@
            
             }        
         }
+        let randomPhrase = getRandomPhraseAsArray(phrases);
+       let randomPhraseLetters = addPhraseToDisplay(randomPhrase);
+
     
-        function checkLetter(para){
+        function checkLetter(button){
             let match = null
-            const checkLetter = document.getElementsByClassName('letter')
-            for(let i=0;i<checkLetter.length;i++){
-                if(checkLetter[i].textContent == para){
-                    checkLetter[i].classList.add('show');
-                    match.textContent =checkLetter[i];
+            const letters = document.getElementsByClassName('letter')
+            let letterPicked = button.textContent;
+
+            for(let i=0;i<letters.length;i++){
+
+                if(letters[i].textContent == letterPicked){
+                    letters[i].classList.add('show');
+                    match =letterPicked;
                     
 
                 }
@@ -46,6 +53,45 @@
             }
             
         
+            qwerty.addEventListener('click', (event)=>{
+               let target = event.target;
+              if(target.tagName='BUTTON' && target.className !== 'chosen'){
+                  target.className=('chosen');
+                  target.disable = true;
+                  const foundLetter = checkLetter(target);
+                  if(foundLetter == null){
+                      let heart = document.querySelector('img[ src="images/liveHeart.png"]')
+                      heart.src="images/lostHeart.png"
+                      missed ++;
+                      
+                  }
+                  checkWin();
+                  
+              }
+            })
+
+
+
+            function checkWin(){
+                const classLi = document.querySelectorAll('.letter');
+                const classShow = document.querySelectorAll('.show');
+                const headline = overlay.firstChild;
+
+                
+                if (missed > 4){
+                    overlay.classList.add('lose');
+                    headline.textContent = 'You Lose :( try again!';
+                    overlay.style.display ='flex'
+                }
+                if(classLi.length === classShow.length){
+                    overlay.classList.add('win');
+                    headline.textContent = 'You won!';
+                    overlay.style.display ='flex'
+
+                }
+
+
+            }
 
         
         
@@ -55,10 +101,8 @@
 
 
 
-    startBtn.addEventListener('click',(event)=>{
-        const target =event.target;
-        const overlay =document.getElementById('overlay');
-
+    startBtn.addEventListener('click',()=>{
+       
         overlay.style.display = 'none';
 
 
